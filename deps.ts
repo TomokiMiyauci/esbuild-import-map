bcb3a53f45c6584b92b9b0ca2ffe54689277a848
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-05-27 15:08:57 +0900

    chore(deps): fix typo

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@ export {
   type ImportMapJson,
   parseFromJson,
 } from "./vendor/deno.land/x/import_map@v0.19.1/mod.ts";
-export { type Plugin } from "npm:esbuild@^0.20.2";
-export { fromFileUrl, isAbsolute, toFileUrl } from "jsr:@std/path@^0.225.1";
-export { escape } from "jsr:@std/regexp@^0.224.1";
-export { distinct } from "jsr:@std/collections@^0.224.1";
+export { type Plugin } from "npm:/esbuild@^0.20.2";
+export { fromFileUrl, isAbsolute, toFileUrl } from "jsr:/@std/path@^0.225.1";
+export { escape } from "jsr:/@std/regexp@^0.224.1";
+export { distinct } from "jsr:/@std/collections@^0.224.1";
