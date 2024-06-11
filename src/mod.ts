dcf0b205af284bdd1adfe2bfd2485cea6e058b01
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2024-06-11 21:25:54 +0900

    refactor(mod): change import source

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,3 @@
-export {
-  type ImportMap,
-  importMapPlugin,
-  type ImportMapResource,
-} from "./plugin.ts";
+export { importMapPlugin, type ImportMapResource } from "./plugin.ts";
 export type { Plugin } from "esbuild";
+export type { ImportMap } from "./types.ts";
